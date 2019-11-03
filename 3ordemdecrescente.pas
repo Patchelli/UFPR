651f7b78f29a3c54368f30342e7e2fa38a099bac
Patchelli <50145785+Patchelli@users.noreply.github.com>
2019-11-03 16:28:52 -0300

    uso de  array

--- 3ordemdecrescente.pas
+++ 3ordemdecrescente.pas
@@ -1,21 +1,35 @@
-Program Pzim ;
-var n1,n2,n3 : integer;
+Program lista1_ex03 ;
+var vet: array[1..3] of integer;
+//var nums: string;
+var e,aux,a,tot: integer;
+var chave : boolean;
 Begin
-  write ('Entre com tres numeros: ');
-  read (n1,n2,n3);
-  if n3>n1 then
-  begin
-    if n1>n2 then write ('A ordem Decrescente: ',n3,' ',n1,' ',n2)
-    else if n2<n3 then write('A ordem Decrescente: ',n3,' ',n2,' ',n1);
-  end;
-  if n1>n2 then
-  begin
-    if n2>n3 then write ('A ordem crescente: ',n1,' ',n2,' ',n3)
-    else if n3<n1 then write('A ordem crescente: ',n1,' ',n3,' ',n2);
-  end;
-  if n1<n2 then
-  begin
-    if n3<n1 then write ('A ordem crescente: ',n2,' ',n1,' ',n3)
-    else if n3<n2 then write('A ordem crescente: ',n2,' ',n3,' ',n1);
-  end;
+  repeat
+    for a:= 1 to 3 do
+    begin
+      writeln('Digite tres numero');
+      readln (vet[a]);
+    end;
+  until(a > 0) ;
+  tot:= 3;
+  repeat
+    tot:= tot - 1;
+    chave:= true;
+    for a:= 1 to tot do
+    if vet[a] > vet[a+1] then
+    begin
+      aux:= vet[a];
+      vet[a]:= vet[a+1];
+      vet[a+1]:= aux;
+      chave:= false;
+    end
+  until (chave);
+  writeln('Ordem Crescente');
+	for a:= 1 to 3 do 
+	write(vet[a],' - ');
+  writeln();
+	writeln('Ordem Decrescente');
+  for a:= 3 downto 1 do
+  write(vet[a],' - ');
+  
 End.
